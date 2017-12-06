cf318fc161707394069fd3f17c2b54ff6d1c2444
syuchan1005 <syuchan.dev@gmail.com>
2017-12-06 18:33:21 +0900

    :construction: (re)support Retina

--- src/com/github/syuchan1005/emojiprefix/EmojiUtil.java
+++ src/com/github/syuchan1005/emojiprefix/EmojiUtil.java
@@ -1,6 +1,11 @@
 package com.github.syuchan1005.emojiprefix;
 
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.reference.SoftReference;
+import com.intellij.spellchecker.FileLoader;
+import com.intellij.util.ResourceUtil;
 import com.intellij.util.ui.UIUtil;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -8,12 +13,14 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import javax.imageio.ImageIO;
@@ -26,45 +33,15 @@ public class EmojiUtil {
 
 	static {
 		try {
-			Icon icon = IconLoader.getIcon("/icons/+1.png");
-			Field myUrl = icon.getClass().getDeclaredField("myUrl");
-			myUrl.setAccessible(true);
-			URL url = (URL) myUrl.get(icon);
-			File parentFile = new File(url.getFile()).getParentFile();
-			if (!parentFile.getParent().endsWith(".jar!")) {
-				String[] emojiList = new File(EmojiUtil.class.getResource("/icons").toURI()).list();
-				assert emojiList != null;
-				for (String emoji : emojiList) {
-					try (InputStream stream = EmojiUtil.class.getResourceAsStream("/icons/" + emoji)) {
-						emojiMap.put(emoji.substring(0, emoji.length() - 4), createIcon(stream));
-					}
-				}
-			} else {
-				String jarPath = parentFile.getParent().replace("file:", "").replace("%20", " ");
-				JarFile jarFile = new JarFile(jarPath.substring(0, jarPath.length() - 1));
-				for (JarEntry jarEntry : Collections.list(jarFile.entries())) {
-					String name = jarEntry.getName();
-					if (name.startsWith("icons/") && name.endsWith(".png")) {
-						try (InputStream stream = jarFile.getInputStream(jarEntry)) {
-							emojiMap.put(name.substring(6, name.length() - 4), createIcon(stream));
-						}
-					}
-				}
+			String[] list = new File(EmojiUtil.class.getResource("/icons").toURI()).list();
+			assert list != null;
+			for (String emoji : list) {
+				if (emoji.contains("@2x")) continue;
+				emojiMap.put(emoji.substring(0, emoji.length() - 4), IconLoader.getIcon("/icons/" + emoji));
 			}
-		} catch (Exception e) {
+		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-	}
-
-	private static Icon createIcon(InputStream stream) throws IOException {
-		int size = UIUtil.isRetina() ? 32 : 16;
-		return new ImageIcon(resizeImage(ImageIO.read(stream), size, size));
-	}
-
-	private static BufferedImage resizeImage(BufferedImage image, int width, int height) {
-		BufferedImage thumb = UIUtil.createImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		thumb.getGraphics().drawImage(image.getScaledInstance(width, height, Image.SCALE_AREA_AVERAGING), 0, 0, width, height, null);
-		return thumb;
 	}
 
 	public static Map<String, Icon> getEmojiMap() {
