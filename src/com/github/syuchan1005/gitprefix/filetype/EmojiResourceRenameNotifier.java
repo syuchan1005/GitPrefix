8cfaaa5bceb531561edf0ee31bd8528960c524e0
syuchan1005 <syuchan.dev@gmail.com>
2018-07-27 19:28:49 +0900

    :heavy_exclamation_mark: Update 1.5.3

--- src/com/github/syuchan1005/gitprefix/filetype/EmojiResourceRenameNotifier.java
+++ src/com/github/syuchan1005/gitprefix/filetype/EmojiResourceRenameNotifier.java
@@ -1,26 +1,17 @@
 package com.github.syuchan1005.gitprefix.filetype;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.components.AbstractProjectComponent;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.event.DocumentEvent;
-import com.intellij.openapi.editor.event.DocumentListener;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.BalloonImpl;
-import com.intellij.ui.components.JBLabel;
 import com.intellij.util.messages.MessageBusConnection;
 import java.io.IOException;
-import javax.swing.BorderFactory;
 import org.jetbrains.annotations.NotNull;
 
 public class EmojiResourceRenameNotifier extends AbstractProjectComponent {
