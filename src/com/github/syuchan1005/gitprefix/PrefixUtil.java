d1ee531f8f48cf055ffd984b495215e14b3f10fe
syuchan1005 <syuchan.dev@gmail.com>
2018-07-31 07:31:19 +0900

    :rocket: Change icon load timing

--- src/com/github/syuchan1005/gitprefix/PrefixUtil.java
+++ src/com/github/syuchan1005/gitprefix/PrefixUtil.java
@@ -16,29 +16,8 @@ import javax.swing.Icon;
 public class PrefixUtil {
 	private static Map<String, Icon> emojiMap = new HashMap<>(890);
 
-	static {
-		Consumer<String> putEmoji = emoji -> {
-			emojiMap.put(emoji, IconLoader.getIcon("/emojis/" + emoji + ".png"));
-		};
-		File file = new File(ResourceUtil.getResource(PrefixUtil.class, "/emojis", "").getFile());
-		String path = file.getParentFile().getPath();
-		if (!path.endsWith(".jar!")) {
-			Arrays.stream(Objects.requireNonNull(file.list()))
-					.filter(name -> !name.endsWith("@2x.png"))
-					.map(name -> name.substring(0, name.length() - 4))
-					.forEach(putEmoji);
-		} else {
-			try {
-				new JarFile(path.substring(6, path.length() - 1)).stream()
-						.filter(entry -> !entry.isDirectory())
-						.map(JarEntry::getName)
-						.filter(entry -> entry.startsWith("emojis/") && !entry.endsWith("@2x.png"))
-						.map(name -> name.substring(7, name.length() - 4))
-						.forEach(putEmoji);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+	private static Icon loadEmoji(String emoji) {
+		return IconLoader.findIcon("/emojis/" + emoji + ".png");
 	}
 
 	public static Map<String, Icon> getEmojiMap() {
@@ -46,6 +25,12 @@ public class PrefixUtil {
 	}
 
 	public static Icon getIcon(String emoji) {
-		return emojiMap.get(emoji);
+		if (emojiMap.containsKey(emoji)) return emojiMap.get(emoji);
+		Icon icon = loadEmoji(emoji);
+		if (icon != null) {
+			emojiMap.put(emoji, icon);
+			return icon;
+		}
+		return null;
 	}
 }
