bd6aa12963e0d4210f5af0b5d331e319aea5e7f4
syuchan1005 <syuchan.dev@gmail.com>
2018-07-30 22:57:27 +0900

    :bug: Commit Window not show

--- src/com/github/syuchan1005/gitprefix/PrefixPanel.java
+++ src/com/github/syuchan1005/gitprefix/PrefixPanel.java
@@ -8,15 +8,16 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.ui.components.JBScrollPane;
-import com.intellij.util.ui.JBUI;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.ButtonGroup;
+import javax.swing.Icon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
-import javax.swing.UIManager;
 
 public class PrefixPanel extends JBScrollPane {
 	private static final String NO_PREFIX = "No Prefix";
@@ -26,54 +27,34 @@ public class PrefixPanel extends JBScrollPane {
 
 	public PrefixPanel(Project project) {
 		JPanel prefixPanel = new JPanel();
-		prefixPanel.setLayout(new VerticalFlowLayout());
+		prefixPanel.setLayout(new VerticalFlowLayout(0, 0, 0, true, false));
 		VirtualFile gitprefix = project.getBaseDir().findChild(".gitprefix");
 		if (gitprefix == null) return;
 		PsiFile psiFile = PsiManager.getInstance(project).findFile(gitprefix);
 		if (psiFile == null) return;
-		int count = 0;
-		for (PsiElement psiElement : psiFile.getChildren()) {
+		PsiElement[] children = psiFile.getChildren();
+		if (children.length == 0) return;
+		for (PsiElement psiElement : children) {
 			if (!(psiElement instanceof PrefixResourceProperty)) continue;
-			prefixPanel.add(createPrefixButton(psiElement.getFirstChild().getText(),
-					psiElement.getFirstChild() == psiElement.getLastChild() ? null : psiElement.getLastChild().getText(), false));
-			count++;
+			prefixPanel.add(createPrefixButton(
+							psiElement.getFirstChild().getText(),
+							psiElement.getFirstChild() == psiElement.getLastChild() ? null : psiElement.getLastChild().getText(),
+							false
+			));
 		}
-		if (count == 0) return;
 		prefixPanel.add(createPrefixButton(null, NO_PREFIX, true));
 		this.setViewportView(prefixPanel);
 		this.setBorder(null);
 		notExists = false;
 	}
 
-	private JRadioButton createPrefixButton(String text, String description, boolean selected) {
-		JRadioButton radioButton = new JRadioButton("", selected);
-		buttonGroup.add(radioButton);
-		int space = UIManager.getIcon("RadioButton.icon").getIconWidth() + radioButton.getIconTextGap();
-		JLabel iconLabel;
-		if (text != null) {
-			if (text.startsWith(":")) {
-				iconLabel = new JLabel(description, PrefixUtil.getIcon(text.replace(":", "")), SwingConstants.CENTER);
-			} else {
-				iconLabel = new JLabel(text.substring(1, text.length() - 1) + (description == null ? "" : " | " + description));
-			}
-		} else {
-			iconLabel = new JLabel(description);
-		}
-		iconLabel.setToolTipText(text);
-		iconLabel.setBorder(JBUI.Borders.emptyLeft(space));
-		radioButton.add(iconLabel);
-		iconLabel.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				radioButton.setSelected(true);
-			}
+	private IconTextRadioButton createPrefixButton(String text, String description, boolean selected) {
+		Icon icon = null;
+		if (text != null && text.startsWith(":")) icon = PrefixUtil.getIcon(text.replace(":", ""));
 
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				radioButton.requestFocus(true);
-			}
-		});
-		return radioButton;
+		IconTextRadioButton iconTextRadioButton = new IconTextRadioButton(description, icon, selected);
+		buttonGroup.add(iconTextRadioButton.getRadioButton());
+		return iconTextRadioButton;
 	}
 
 	public boolean notExists() {
@@ -82,5 +63,60 @@ public class PrefixPanel extends JBScrollPane {
 
 	public ButtonGroup getButtonGroup() {
 		return buttonGroup;
+	}
+
+	class IconTextRadioButton extends JPanel {
+		private JRadioButton radioButton;
+		private JLabel label;
+
+		public IconTextRadioButton(String text, Icon icon, boolean selected) {
+			super(new FlowLayout(FlowLayout.LEADING, 5, 1));
+			this.radioButton = new JRadioButton("", selected);
+
+			this.label = new JLabel(text, icon, SwingConstants.CENTER);
+			label.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					radioButton.setSelected(!radioButton.isSelected());
+				}
+
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					radioButton.requestFocus(true);
+				}
+			});
+
+			this.add(this.radioButton);
+			this.add(this.label);
+		}
+
+		public void addActionListener(ActionListener var1) {
+			this.radioButton.addActionListener(var1);
+		}
+
+		public JRadioButton getRadioButton() {
+			return radioButton;
+		}
+
+		public JLabel getLabel() {
+			return label;
+		}
+
+		public boolean isSameAs(Object var1) {
+			return this.radioButton == var1;
+		}
+
+		public void setEnabled(boolean var1) {
+			this.radioButton.setEnabled(var1);
+			this.label.setEnabled(var1);
+		}
+
+		public boolean isSelected() {
+			return this.radioButton.isSelected();
+		}
+
+		public void setSelected(boolean var1) {
+			this.radioButton.setSelected(var1);
+		}
 	}
 }
