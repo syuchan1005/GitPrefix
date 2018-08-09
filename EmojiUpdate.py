075d39c3734a4754b582172888f1b4425027fc13
syuchan1005 <syuchan.dev@gmail.com>
2018-08-09 22:45:51 +0900

    :construction: change path substring value

--- EmojiUpdate.py
+++ EmojiUpdate.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
     for file in list:
         if file[-3:] == "png":
             os.system('ffmpeg -y -i ./emoji-cheat-sheet/public/graphics/emojis/' + file + " -loglevel fatal -vf scale=16:16 ./resources/emojis/" + file)
+            # os.system('ffmpeg -y -i ./emoji-cheat-sheet/public/graphics/emojis/' + file + " -loglevel fatal -vf scale=16:16 ./resources/emojis/" + file.replace(".png", "_dark.png"))
             os.system('ffmpeg -y -i ./emoji-cheat-sheet/public/graphics/emojis/' + file + " -loglevel fatal -vf scale=32:32 ./resources/emojis/" + file.replace(".png", "@2x.png"))
+            # os.system('ffmpeg -y -i ./emoji-cheat-sheet/public/graphics/emojis/' + file + " -loglevel fatal -vf scale=32:32 ./resources/emojis/" + file.replace(".png", "@2x_dark.png"))
             print(str(c) + "/" + l)
             c += 1
--- src/com/github/syuchan1005/gitprefix/PrefixUtil.java
+++ src/com/github/syuchan1005/gitprefix/PrefixUtil.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.util.IconLoader;
 import com.intellij.util.ResourceUtil;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +13,7 @@ import java.util.function.Consumer;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import javax.swing.Icon;
+import org.apache.commons.lang.SystemUtils;
 
 public class PrefixUtil {
 	private static Map<String, Icon> emojiMap = new HashMap<>(890);
@@ -20,7 +22,8 @@ public class PrefixUtil {
 		Consumer<String> putEmoji = emoji -> {
 			emojiMap.put(emoji, IconLoader.getIcon("/emojis/" + emoji + ".png"));
 		};
-		File file = new File(ResourceUtil.getResource(PrefixUtil.class, "/emojis", "").getFile());
+		URL resource = ResourceUtil.getResource(PrefixUtil.class, "/emojis", "");
+		File file = new File(resource.getFile());
 		String path = file.getParentFile().getPath();
 		if (!path.endsWith(".jar!")) {
 			Arrays.stream(Objects.requireNonNull(file.list()))
@@ -29,7 +32,7 @@ public class PrefixUtil {
 							.forEach(putEmoji);
 		} else {
 			try {
-				new JarFile(path.substring(6, path.length() - 1)).stream()
+				new JarFile(path.substring(SystemUtils.IS_OS_WINDOWS ? 6 : 5, path.length() - 1)).stream()
 								.filter(entry -> !entry.isDirectory())
 								.map(JarEntry::getName)
 								.filter(entry -> entry.startsWith("emojis/") && !entry.endsWith("@2x.png"))
