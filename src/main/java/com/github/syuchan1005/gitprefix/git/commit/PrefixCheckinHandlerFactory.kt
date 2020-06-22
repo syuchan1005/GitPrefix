ccc3809e362ced5ad097bf95933a7b6e6595badb
syuchan1005 <syuchan.dev@gmail.com>
2020-06-23 02:03:14 +0900

    :construction: support 202

--- src/main/java/com/github/syuchan1005/gitprefix/git/commit/PrefixCheckinHandlerFactory.kt
+++ src/main/java/com/github/syuchan1005/gitprefix/git/commit/PrefixCheckinHandlerFactory.kt
@@ -1,11 +1,12 @@
 package com.github.syuchan1005.gitprefix.git.commit
 
 import com.intellij.openapi.vcs.CheckinProjectPanel
+import com.intellij.openapi.vcs.changes.CommitContext
 import com.intellij.openapi.vcs.checkin.CheckinHandler
 import git4idea.checkin.GitCheckinHandlerFactory
 
 class PrefixCheckinHandlerFactory : GitCheckinHandlerFactory() {
-    override fun createVcsHandler(panel: CheckinProjectPanel): CheckinHandler {
+    override fun createVcsHandler(panel: CheckinProjectPanel, commitContext: CommitContext): CheckinHandler {
         return PrefixCheckinHandler(panel)
     }
 }
