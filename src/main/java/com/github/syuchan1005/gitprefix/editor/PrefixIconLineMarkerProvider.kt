efee4a4628e06cea671620f775417e6e8d58e7da
syuchan1005 <syuchan.dev@gmail.com>
2020-07-08 19:20:05 +0900

    :tada: v2.1.10

--- src/main/java/com/github/syuchan1005/gitprefix/editor/PrefixIconLineMarkerProvider.kt
+++ src/main/java/com/github/syuchan1005/gitprefix/editor/PrefixIconLineMarkerProvider.kt
@@ -1,33 +1,28 @@
-package com.github.syuchan1005.gitprefix.editor;
+package com.github.syuchan1005.gitprefix.editor
 
-import com.github.syuchan1005.gitprefix.EmojiUtil;
-import com.github.syuchan1005.gitprefix.grammar.psi.PrefixResourceTypes;
-import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
-import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
-import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.impl.source.tree.LeafPsiElement;
-import java.util.Collection;
-import org.jetbrains.annotations.NotNull;
+import com.github.syuchan1005.gitprefix.EmojiUtil.Companion.emojiMap
+import com.github.syuchan1005.gitprefix.grammar.psi.PrefixResourceTypes
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
+import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.LeafPsiElement
 
-public class PrefixIconLineMarkerProvider extends RelatedItemLineMarkerProvider {
-
-	@Override
-	protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo<?>> result) {
-		if (element instanceof LeafPsiElement) {
-			LeafPsiElement leafPsiElement = (LeafPsiElement) element;
-			RelatedItemLineMarkerInfo<PsiElement> lineMarkerInfo = null;
-			if (leafPsiElement.getElementType() == PrefixResourceTypes.KEY_TEXT &&
-				leafPsiElement.getNode().getTreePrev().getElementType() == PrefixResourceTypes.EMOJI_WRAP) {
-				String emoji = leafPsiElement.getText();
-				if (emoji.length() < 1) return;
-				EmojiUtil.EmojiData emojiData = EmojiUtil.getEmojiMap().get(emoji);
-				if (emojiData != null) {
-					NavigationGutterIconBuilder<PsiElement> builder = NavigationGutterIconBuilder.create(emojiData.getIcon()).setTarget(element);
-					lineMarkerInfo = builder.createLineMarkerInfo(element);
-				}
-			}
-			if (lineMarkerInfo != null) result.add(lineMarkerInfo);
-		}
-	}
+class PrefixIconLineMarkerProvider : RelatedItemLineMarkerProvider() {
+    override fun collectNavigationMarkers(element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<*>?>) {
+        if (element is LeafPsiElement) {
+            var lineMarkerInfo: RelatedItemLineMarkerInfo<PsiElement?>? = null
+            if (element.elementType === PrefixResourceTypes.KEY_TEXT &&
+                    element.node.treePrev.elementType === PrefixResourceTypes.EMOJI_WRAP) {
+                val emoji = element.text
+                if (emoji.length < 1) return
+                val emojiData = emojiMap[emoji]
+                if (emojiData != null) {
+                    val builder = NavigationGutterIconBuilder.create(emojiData.icon).setTarget(element)
+                    lineMarkerInfo = builder.createLineMarkerInfo(element)
+                }
+            }
+            if (lineMarkerInfo != null) result.add(lineMarkerInfo)
+        }
+    }
 }
